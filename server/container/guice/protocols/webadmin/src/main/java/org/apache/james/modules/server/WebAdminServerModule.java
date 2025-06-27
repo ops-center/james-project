f2ba856d79f38472bdf5849c0ae28ddb65b4d61d
Samiul Islam <samiulsami7786@gmail.com>
2025-06-27 12:49:06 +0600

    Squash #16 (All commits as of May 12th 2025) (#26)

--- server/container/guice/protocols/webadmin/src/main/java/org/apache/james/modules/server/WebAdminServerModule.java
+++ server/container/guice/protocols/webadmin/src/main/java/org/apache/james/modules/server/WebAdminServerModule.java
@@ -40,6 +40,7 @@ import org.apache.james.server.task.json.TaskExtensionModule;
 import org.apache.james.server.task.json.dto.AdditionalInformationDTO;
 import org.apache.james.server.task.json.dto.AdditionalInformationDTOModule;
 import org.apache.james.task.TaskExecutionDetails;
+import org.apache.james.user.api.UsersRepository;
 import org.apache.james.utils.ClassName;
 import org.apache.james.utils.ExtensionConfiguration;
 import org.apache.james.utils.GuiceLoader;
@@ -185,11 +186,11 @@ public class WebAdminServerModule extends AbstractModule {
     @Singleton
     public AuthenticationFilter providesAuthenticationFilter(PropertiesProvider propertiesProvider,
                                                              WebAdminConfiguration webAdminConfiguration,
-                                                             @Named("webadmin") JwtTokenVerifier.Factory jwtTokenVerifier) throws Exception {
+                                                             @Named("webadmin") JwtTokenVerifier.Factory jwtTokenVerifier, UsersRepository usersRepository) throws Exception {
         try {
             Configuration configurationFile = propertiesProvider.getConfiguration("webadmin");
             if (configurationFile.getBoolean("jwt.enabled", DEFAULT_JWT_DISABLED)) {
-                return new JwtFilter(jwtTokenVerifier);
+                return new JwtFilter(jwtTokenVerifier, usersRepository);
             }
             return webAdminConfiguration.getPassword()
                 .<AuthenticationFilter>map(PasswordFilter::new)
